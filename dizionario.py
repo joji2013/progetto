c5b6d44eb57a6fd709fd64653e130baedd151c4d
joji2013 <pantarbr@yahoo.com>
2025-10-11 13:40:26 +0200

    Create dizionario.py
    serve per capire delle nuove parole

---
+++ dizionario.py
@@ -0,0 +1,13 @@
+None
+for i in range (4):
+    parola = input("c'è qualche parola che che non capisci")
+    if parola == "cringe":
+        print("vuol dire imbarazante")
+    elif parola == "lol":
+        print("vuol dire divertito")
+    elif parola == "top":
+        print(" vuol dire il massimo")
+    elif parola == "bro":
+        print("vuol dire amico")
+    else:
+        print("non esite")
